9776c43ae3ebd625f394864a7b4f8e87cec57de9
caiomcs04 <caio6c@yahoo.com.br>
2022-06-16 11:05:23 -0300

    2#Commit(service and login page)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { HomeComponent } from './home.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { LoginComponent } from './login/login.component';
+
+
+@NgModule({
+  declarations: [HomeComponent, LoginComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ],
+  exports:[HomeComponent]
+})
+export class HomeModule { }
